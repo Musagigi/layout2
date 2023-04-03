18b0c7b09f5dea72b07a30d0555da764df1691b7
Musajiji <dzhal@bk.ru>
2023-04-03 22:29:55 +0300

    добавляю js

---
+++ build/js/main.js
@@ -0,0 +1,11 @@
+window.addEventListener('load', function () {
+	const previewVideo = document.querySelector('.previewVideo')
+
+	previewVideo.addEventListener('click', function (event) {
+		console.log(event);
+		if (event.target.contains('circle')) {
+			console.log('test');
+		}
+	})
+})
+//# sourceMappingURL=data:application/json;charset=utf8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIm1haW4uanMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IkFBQUE7QUFDQTtBQUNBO0FBQ0E7QUFDQTtBQUNBO0FBQ0E7QUFDQTtBQUNBO0FBQ0EiLCJmaWxlIjoibWFpbi5qcyIsInNvdXJjZXNDb250ZW50IjpbIndpbmRvdy5hZGRFdmVudExpc3RlbmVyKCdsb2FkJywgZnVuY3Rpb24gKCkge1xuXHRjb25zdCBwcmV2aWV3VmlkZW8gPSBkb2N1bWVudC5xdWVyeVNlbGVjdG9yKCcucHJldmlld1ZpZGVvJylcblxuXHRwcmV2aWV3VmlkZW8uYWRkRXZlbnRMaXN0ZW5lcignY2xpY2snLCBmdW5jdGlvbiAoZXZlbnQpIHtcblx0XHRjb25zb2xlLmxvZyhldmVudCk7XG5cdFx0aWYgKGV2ZW50LnRhcmdldC5jb250YWlucygnY2lyY2xlJykpIHtcblx0XHRcdGNvbnNvbGUubG9nKCd0ZXN0Jyk7XG5cdFx0fVxuXHR9KVxufSkiXX0=
