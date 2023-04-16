f67f69c958a4b6c1395dbb4d2ef090521d5270da
Musajiji <dzhal@bk.ru>
2023-04-16 23:28:43 +0300

    -

--- src/js/slider.js
+++ src/js/slider.js
@@ -1,35 +1,35 @@
-// const slider = document.querySelector('.slider')
-// const photo = document.querySelectorAll('.slider__photo')
-// const btnLeft = document.querySelector('.sliderBtn__left')
-// const btnRight = document.querySelector('.sliderBtn__right')
-// let current = 0
+const slider = document.querySelector('.slider')
+const photo = document.querySelectorAll('.slider__photo')
+const btnLeft = document.querySelector('.sliderBtn__left')
+const btnRight = document.querySelector('.sliderBtn__right')
+let current = 0
 
-// slider.addEventListener('contextmenu', cancelEvent)
-// slider.addEventListener('mousedown', cancelEvent)
+slider.addEventListener('contextmenu', cancelEvent)
+slider.addEventListener('mousedown', cancelEvent)
 
-// function cancelEvent(event) {
-// 	event.preventDefault()
-// }
+function cancelEvent(event) {
+	event.preventDefault()
+}
 
-// btnLeft.addEventListener('click', function () {
-// 	photo[current].classList.remove('slider__photo_show')
+btnLeft.addEventListener('click', function () {
+	photo[current].classList.remove('slider__photo_show')
 
-// 	current--
+	current--
 
-// 	if (current < 0) {
-// 		current = photo.length - 1
-// 	}
-// 	photo[current].classList.add('slider__photo_show')
-// })
+	if (current < 0) {
+		current = photo.length - 1
+	}
+	photo[current].classList.add('slider__photo_show')
+})
 
-// btnRight.addEventListener('click', function () {
-// 	photo[current].classList.remove('slider__photo_show')
+btnRight.addEventListener('click', function () {
+	photo[current].classList.remove('slider__photo_show')
 
-// 	current++
+	current++
 
-// 	if (current === photo.length) {
-// 		current = 0
-// 	}
-// 	photo[current].classList.add('slider__photo_show')
-// })
+	if (current === photo.length) {
+		current = 0
+	}
+	photo[current].classList.add('slider__photo_show')
+})
 
