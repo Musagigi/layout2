5b85509f6d2da10767688ab71f7b23563152bbbf
Musajiji <dzhal@bk.ru>
2023-04-17 08:03:13 +0300

    -

--- src/js/headerMenu.js
+++ src/js/headerMenu.js
@@ -1,11 +1,21 @@
-const burger = document.querySelector('.burger')
-const middleLine = document.querySelector('.middleLine')
-const navigateList = document.querySelector('.navigate')
+const body = document.querySelector('.body')
+const burger = body.querySelector('.burger')
+const middleLine = burger.querySelector('.middleLine')
+const navigateList = body.querySelector('.navigate')
 
 burger.addEventListener('click', function () {
 	navigateList.classList.toggle('navigate_active')
 	middleLine.classList.toggle('middleLine_active')
 	burger.classList.toggle('burger_transparent')
+})
+
+body.addEventListener('click', function (event) {
+
+	if (!event.target.closest('.burger')) {
+		navigateList.classList.remove('navigate_active')
+		middleLine.classList.remove('middleLine_active')
+		burger.classList.remove('burger_transparent')
+	}
 })
 
 const searchBtn = document.querySelector('.dropdown__searchBtn')
